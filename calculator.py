daa6bc131a7291ebe7c4e971b97f29b10c38de64
myUsername806 <aidanoronha20@gmail.com>
2025-08-08 13:09:44 -0300

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1= float (input("enter the first number: "))
+num2 =float (input("enter the second number: "))
+
+sum_result = num1 + num2
+
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+
+print (f"sum: {sum_result}") # +
+
+print (f"difference: {difference_result}") #  -
+
+print (f"product: {product_result}") # ×
+
+print (f"quotient: {quotient_result}") # ÷
